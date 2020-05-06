612b6f206e577bb17ba044c455169b2e17f80e10
sheuli <mondalsheuli07@gmail.com>
2020-05-07 02:13:11 +0530

    Creating

---
+++ Concept/Fundametals/Part1/varLetConst.js
@@ -0,0 +1,16 @@
+var name = 'Sheuli Mondal' 
+const IAM = 'Mana'
+let a = 10;
+
+name = 'Priyanka'
+
+function abc(){
+    let a = 5
+    console.log(a);
+}
+
+abc();
+console.log(a);
+
+console.log(name);
+console.log(IAM);
